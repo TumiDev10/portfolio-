724fe874eb47390862c37d518d5a96f2cc0acae8
Tumi Mashigo <tumi.mashigo15@gmail.com>
2023-06-15 09:25:40 +0200

    added style

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,11 +1,65 @@
-import React from "react";
+import React from 'react';
+import './Projects.css';
+
+const projects = [
+  {
+    name: 'Project 1',
+    description: 'A description of your project.',
+    link: '#'
+  },
+  {
+    name: 'Project 2',
+    description: 'A description of your project.',
+    link: '#'
+  },
+  {
+    name: 'Project 3',
+    description: 'A description of your project.',
+    link: '#'
+  }
+];
+
+const githubRepos = [
+  {
+    name: 'Repo 1',
+    link: 'https://github.com/your-username/repo1'
+  },
+  {
+    name: 'Repo 2',
+    link: 'https://github.com/your-username/repo2'
+  },
+  {
+    name: 'Repo 3',
+    link: 'https://github.com/your-username/repo3'
+  }
+];
 
 function Projects() {
-    return(
-        <div>
-            <h1>Projects</h1>
-        </div>
-    )
+  return (
+    <div className="App">
+      <h1 style={{marginTop: '180px'}} class="section-title">
+        Recent 
+        <span>Projects</span>
+      </h1>
+      <div className="projects-container">
+        {projects.map((project, index) => (
+          <div className="project-card" key={index}>
+            <h2>{project.name}</h2>
+            <p>{project.description}</p>
+            <a href={project.link}>View Project</a>
+          </div>
+        ))}
+      </div>
+      <div className="github-repos">
+        <h2>GitHub Repositories</h2>
+        {githubRepos.map((repo, index) => (
+          <div className="github-repo" key={index}>
+            <a href={repo.link}>{repo.name}</a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
--- src/Components/Services.js
+++ src/Components/Services.js
@@ -8,13 +8,13 @@ function Services() {
             <section id="services">
                 <div class="services container">
                     <div class="service-top">
-                        <h1 class="section-title">
+                        <h1 style={{marginTop: '20%'}} class="section-title">
                             Serv
                             <span>i</span>
                             ces
                         </h1>
                         <p>
-                             My web design service provides you with a customized website design that's perfect for your business or personal site. I will be working with you to create a design that matches your brand, while giving your customers a seamless user experience that will delight them! 
+                        As a full stack developer, I provide comprehensive web development services to meet all your project requirements. With expertise in both front-end and back-end development, I ensure a seamless and efficient development process from start to finish. Here's what you can expect from my services:
                         </p>
                     </div>
                     <div class="service-bottom">
@@ -23,11 +23,66 @@ function Services() {
                                 <img src="https://img.icons8.com/bubbles/100/000000/services.png"></img>
 
                             </div>
-                            <h2>Android applications development services</h2>
+                            <h2>Captivating Front-end development</h2>
                             <p>
-                                The number of people who own Android devices is growing year over year. You certainly want to offer your unique solution to this wide user base! I develop Android apps for various devices using the Java and Kotlin programming languages and Google’s own development tools and guidelines. 
+                            I create visually appealing and user-friendly interfaces using the latest web technologies like React.js, React Native, HTML, CSS, and JavaScript.
+                            Your website will be responsive and optimized for various devices and screen sizes, ensuring a consistent experience for your users.
+                            I bring your web pages to life with interactive features, animations, and transitions, enhancing user engagement.
                             </p>
                         </div>
+
+                        <div class="service-item">
+                            <div class="icon">
+                                <img src="https://img.icons8.com/bubbles/100/000000/services.png"></img>
+                            </div>
+                            <h2>Powerful Back-end Development:</h2>
+                            <p>
+                            I develop robust server-side applications using languages like Java, or Node.js, ensuring efficient handling of your business logic.
+                            Database management is my expertise, and I design and implement scalable databases with proper data modeling and querying techniques.
+                            I seamlessly integrate RESTful APIs to facilitate smooth communication between your front-end and back-end components.
+                            Security is a priority, and I implement measures to protect your application against common vulnerabilities. 
+                            </p>
+                        </div>
+
+                        <div class="service-item">
+                            <div class="icon">
+                                <img src="https://img.icons8.com/bubbles/100/000000/services.png"></img>
+                            </div>
+                            <h2>Efficient Database Management:</h2>
+                            <p>
+                            I design and set up databases based on your specific requirements, utilizing popular database systems such as MySQL, or MongoDB.
+                            From creating schemas and tables to optimizing complex queries, I ensure your data is structured and easily accessible.
+                            I handle data migration, backup, and recovery strategies to safeguard your valuable information.
+                            </p>
+                        </div>
+
+                        <div class="service-item">
+                            <div class="icon">
+                                <img src="https://img.icons8.com/bubbles/100/000000/services.png"></img>
+                            </div>
+                            <h2>Collaborative Version Control and Deployment:</h2>
+                            <p>
+                            I utilize version control systems like Git or Buddy to manage and track changes, allowing seamless collaboration with your team.
+                            With platforms like GitHub or GitLab, I ensure efficient code sharing, conflict resolution, and smooth code merging.
+                            </p>
+                            <p></p>
+                        </div>
+
+                        <div class="service-item">
+                            <div class="icon">
+                                <img src="https://img.icons8.com/bubbles/100/000000/services.png"></img>
+                            </div>
+                            <h2>Effective Problem Solving and Troubleshooting:</h2>
+                            <p>
+                            I excel at identifying and resolving issues in both front-end and back-end components, ensuring your application runs smoothly.
+                            From debugging code and optimizing performance to addressing security vulnerabilities, I take care of the technical challenges.
+                            Through thorough testing, including unit testing, integration testing, and end-to-end testing, I ensure a reliable and bug-free application.
+                            I continuously analyze and improve the architecture and performance of your application to meet your evolving needs.
+                            </p>
+                            <p></p>
+                        </div>
+
+
                     </div>
                 </div>
             </section>
